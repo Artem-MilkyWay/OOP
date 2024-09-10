d25c71ccb0b034bc7bd1a2e1d79dbbf6826b5d41
Artem <mikiyanskiy@mail.ru>
2024-09-11 00:11:04 +0700

    sample

---
+++ Task_1_1_1/src/test/java/ru/nsu/mikiyanskiy/MainTest.java
@@ -0,0 +1,10 @@
+package ru.nsu.mikiyanskiy;
+import  org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test new*
+    void Sample(){
+        assertEquals(3,Main.sum(1,2));
+    }
+}
