51baa6986b15565f2c87de56956ccb0cb686d9f3
Artem <54033048+Artem-MilkyWay@users.noreply.github.com>
2024-09-30 12:16:32 +0700

    Update and rename Main.java to HeapSort.java

--- Task_1_1_1/src/main/java/ru/nsu/mikiyanskiy/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/mikiyanskiy/HeapSort.java
@@ -2,7 +2,7 @@ package ru.nsu.mikiyanskiy;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class HeapSort {
 
     // Основная функция heapSort
     public static void heapSort(int[] arr) {
