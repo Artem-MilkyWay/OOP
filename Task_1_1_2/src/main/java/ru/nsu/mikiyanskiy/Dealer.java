b33bb611f2ee3f1011363f18edacbbd29e67e0c8
Artem <54033048+Artem-MilkyWay@users.noreply.github.com>
2024-10-12 20:19:40 +0700

    Update Dealer.java

--- Task_1_1_2/src/main/java/ru/nsu/mikiyanskiy/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/mikiyanskiy/Dealer.java
@@ -1,14 +1,23 @@
 package ru.nsu.mikiyanskiy;
 
+/**
+ * класс Дилера
+ */
 public class Dealer extends Player {
     public Dealer() {
         super("Dealer");
     }
 
-    public void showInitialHand() {   // вывод положения дилера в игре до открытия второй карты
+    /**
+     * вывод положения дилера в игре до открытия второй карты
+     */
+    public void showInitialHand() {   
         System.out.println("    Dealer's cards: " + "["+hand.get(0) + ", <closed card>"+"]");
     }
 
+    /**
+     * полноценный вывод
+     */
     public void showHand(){
         System.out.println("    Dealer's cards: " + hand + " => " + getScore());
     }
