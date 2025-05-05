10a0d9aece43feef73779f7925418e7df5036ad3
Artem-MilkyWay <Artem-MilkyWay@users.noreply.github.com>
2025-05-05 07:39:23 +0000

    Deploying to gh-pages from @ Artem-MilkyWay/OOP@20475c1a87f22fad938bac183df06e0ed889f64f 🚀

---
+++ Task_2_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.service","l":"BuildChecker"},{"p":"org.example.model","l":"BuildResult"},{"p":"org.example.service","l":"HTMLGenerator"},{"p":"org.example","l":"Main"},{"p":"org.example.service","l":"RepositoryCloner"},{"p":"org.example.model","l":"Student"}];updateSearchResults();
